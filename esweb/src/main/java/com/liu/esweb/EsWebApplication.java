3bdebb058bf83fca191b4f3964982ce6526c273c
shengbinliu <203003747@qq.com>
2021-07-20 14:06:30 +0800

    feat 提交框架

---
+++ esweb/src/main/java/com/liu/esweb/EsWebApplication.java
@@ -0,0 +1,16 @@
+package com.liu.esweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author shengbinliu
+ */
+@SpringBootApplication
+public class EsWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EsWebApplication.class, args);
+    }
+
+}
---
+++ esweb/src/test/java/com/liu/esweb/EswebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liu.esweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EswebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
